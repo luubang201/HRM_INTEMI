5db5d55c42ef2c7bb4fc17e103c3335a472aa967
Luu <luubang201@gmail.com>
2023-04-29 08:16:13 +0700

    push code

---
+++ resources/js/Pages/Dashboard/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="mb-8 font-bold text-3xl">Trang Chủ</h1>
+    <p class="mb-8 leading-normal">Chào mừng đã đến với website quản lý nhân sự, đồ án năm 4 được thực hiện bởi <a class="text-indigo-500 underline hover:text-orange-600" href="https://ha-noi.congtydoanhnghiep.com/cong-ty-co-phan-sang-tao-cong-nghe-intemi-viet-nam#:~:text=Th%C3%B4ng%20tin%20C%C3%B4ng%20Ty%20C%E1%BB%95%20Ph%E1%BA%A7n%20S%C3%A1ng%20T%E1%BA%A1o,Ph%C3%BA%20Di%E1%BB%85n%2C%20Qu%E1%BA%ADn%20B%E1%BA%AFc%20T%E1%BB%AB%20Li%C3%AAm%2C%20H%C3%A0%20N%E1%BB%99i.">Nguyễn Hồng Phong</a></p>
+  </div>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+
+export default {
+  metaInfo: { title: 'Dashboard' },
+  layout: Layout,
+}
+</script>
